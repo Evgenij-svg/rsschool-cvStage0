ba6ab89a621f44e242cb64ede9ebdc7580dceab1
Evgenij-svg <deshkevich.evgenij@gmail.com>
2022-08-04 01:58:55 +0300

    fix: fix bugs

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,14 @@ const btn = document.getElementById('BurgerID');
 const Brightness=document.querySelectorAll('.Brightness');
 const CloseBurger = document.querySelectorAll('#CloseBurger');
 btn.onclick = function() {
-    document.getElementById('BurgerMenuId').style.left = '225px';
+    document.getElementById('BurgerMenuId').style.top = '0px';
     Brightness.forEach(Elem=>{
       Elem.style.filter='brightness(60%)'; 
     })
   }
   CloseBurger.forEach(CloseBurger=>{
     CloseBurger.onclick=function(){
-      document.getElementById('BurgerMenuId').style.left = '600px';
+      document.getElementById('BurgerMenuId').style.top = '-300px';
       Brightness.forEach(Elem=>{
         Elem.style.filter='brightness(100%)'; 
       })
